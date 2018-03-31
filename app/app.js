b838e35cce56252074cb367b345a3d3bdff470a3
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-03-30 21:38:13 -0300

    #1 incluindo campos no banco para serem populados
    Signed-off-by: Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>

--- app/app.js
+++ app/app.js
@@ -2,3 +2,8 @@ var express = require('express');
 var mongo = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
