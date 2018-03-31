2fa50ce32a591c903bab5d68a9092617a085e77c
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-03-30 22:44:09 -0300

    #2 Adicionando campos no banco de dados
    Signed-off-by: Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>

--- editarGestor/app/app.js
+++ editarGestor/app/app.js
@@ -2,3 +2,20 @@ var express = require('express');
 var mongo = require('mongoose');
 var bodyParser = require('bodyParser');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser({
+    extended = true
+}));
+
+var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + '27017/testeapi';
+console.log(mongoaddr);
+mongo.connect(mongoaddr);
+
+var taskListSchema = mongo.Schema({
+    nome = { type : String},
+    matricula = { type : String },
+    setor = { type : String },
+    senha = { type : String},
+    hospital = { type: String },
+});
