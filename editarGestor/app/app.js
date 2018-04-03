3424a40e223c1c7b450946d505da6adacae5ec26
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-04-03 10:55:08 -0300

    Fix #10 Correcão de alguns bugs encontrados no código
    Signed-off-by: Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>

--- editarGestor/app/app.js
+++ editarGestor/app/app.js
@@ -1,63 +1,53 @@
+//Exemplo de Web Service REST utilizando NodeJS e MongoDB em Containers Docker
+//
 var express = require('express');
 var mongo = require('mongoose');
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser({
-    extended = true
+app.use(bodyParser.urlencoded({
+    extended: true
 }));
 
-var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + '27017/testeapi';
+//Conexão com o MongoDB
+var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/testeapi';
 console.log(mongoaddr);
 mongo.connect(mongoaddr);
 
+//Esquema da collection do Mongo
 var taskListSchema = mongo.Schema({
-    nome = { type : String},
-    matricula = { type : String },
-    setor = { type : String },
-    senha = { type : String},
-    hospital = { type: String },
+	descricao : { type: String },
+	concluido : Boolean,
+	updated_at: { type: Date, default: Date.now },
 });
 
-var Model = mongo.model('Tasks', taskListSchema);
+//Model da aplicação
+var Usuario = mongo.model('Tasks', taskListSchema);
 
-//Visualizar gestor
-
-app.get("/api/:id", function(req, res){
-    Model.find(req.params.id, req.body, function (err, post){
-
-        if (err) {
+//GET - Retorna todos os gestores existentes no banco
+app.get("/api/all", function (req, res) {
+	Usuario.find(function(err, todos) {
+		if (err) {
 			res.json(err);
 		} else {
-			res.json(post);
+			res.json(todos);
 		}
-    });
-
+	})
 });
-
-//Atualizar atualizar
 
 app.put("/api/put/:id", function(req, res) {
 
     Model.findByIdAndUpdate(req.params.id, req.body ,function(err, post) {
 
-        'nome' = req.body.nome,
-        'matricula' = req.body.matricula
-        'setor' = req.body.setor,
-        'senha' = req.body.senha,
-        'hospital' = req.body.hospital
-
         if (err) return next(err);
-        res.json(post);
-        res.json({ message: 'Atualizado !!' });
+        	res.json(post);
+        	res.json({ message: 'Atualizado !!' });
 
     });
  });
 
-
+//Porta de escuta do servidor
 app.listen(8080, function() {
 	console.log('Funcionando');
 });
-
- //Finalizando API para atualizar
