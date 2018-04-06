fa5103e3d3bf0db30721cdfa00589dcfd4a17d70
Vitor Leal <vitinho2201@gmail.com>
2018-04-05 23:35:47 -0300

    #9 Finalizando a API
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    
    Co-authored-by: Vitor Leal <vitinho2201@gmail.com>
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>

--- login/app/app.js
+++ login/app/app.js
@@ -5,6 +5,7 @@ var app = express();
 var morgan = require('morgan');
 var User = require('./user');
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -15,61 +16,53 @@ var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/t
 console.log(mongoaddr);
 mongo.connect(mongoaddr);
 
-
-
 app.post("/api/add", function (req, res) {
 
+  bcrypt.hash(req.body.password, 10, function (err, hash){
 
-
+    if (err) {
+      return next(err);
+    }
 		var register = new User({
 			'name' : req.body.name,
 			'registration' : req.body.registration,
-			'password' : req.body.password,
+			'password' : hash,
 			'manager' : req.body.manager
-		});
-	if (req.body.registration && req.body.password) {
+      });
+
 		register.save(function (err) {
 
 			if (err) {
 				console.log(err);
-				res.send(err);
 				res.end();
 			}
-
 		});
-	}
 	res.send(register);
 	res.end();
 
-
+  });
 });
 
 
 app.post('/authenticate',function(req,res){
-  User.findOne({name: req.body.name}, function(err,user){
-    if(err) throw err;
-
-    if(!user){
-      res.json({success:false, message:'Authentication failed! User not found.'});
-    }
-    else if(user){
-      if(user.password != req.body.password){
-		res.json({
-			success:false,
-			message:'Authentication failed! Invalid password.'
-		});
-      } else {
-		var token = jwt.sign({name:user.name}, app.get('superSecret'),{ expiresInMinutes: 1440});
-		res.json({
-		  success: true,
-		  message: 'Enjoy your token!',
-		  token: token
-		});
+  User.findOne({ registration: req.body.registration})
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('Usuario nao enco');
+        err.status = 401;
+        return console.log("Erro!")
       }
-    }
-  });
+      bcrypt.compare(req.body.password , user.password, function (err, result) {
+        if (result === true) {
+          return console.log("Usuario logado!!");
+        } else {
+          return console.log("Erro! Senha invalida!");
+        }
+      })
+    });
 });
-
 
 app.listen(8080,function(){
   console.log('Funcionando!');
