a97bc216d702cf38ed08f69dcb8cb6e2cea0450b
Guilherme Deusdará <guibanci@gmail.com>
2018-04-07 12:37:17 -0300

    HOT FIX #9 arrumando erro de requisição
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Vitor Leal  <vitinho2201@gmail.com>
    
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Vitor Leal  <vitinho2201@gmail.com>

--- login/app/app.js
+++ login/app/app.js
@@ -22,7 +22,9 @@ app.post("/api/add", function (req, res) {
   bcrypt.hash(req.body.password, 10, function (err, hash){
 
     if (err) {
-      return next(err);
+      return res.json({
+        message: 'password not found.'
+      });
     }
 		var register = new User({
 			'name' : req.body.name,
