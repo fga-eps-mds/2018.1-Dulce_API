8b250e83d2bc9cca49a1138c63876cb207e38d6d
Guilherme Deusdará <guibanci@gmail.com>
2018-04-11 00:01:37 -0300

    #17 refatorando model e newUser
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- User/app/Components/newUser.js
+++ User/app/Components/newUser.js
@@ -1,42 +1,25 @@
 var User = require('./model/user');
-var bcrypt = require('bcrypt');
 
 module.exports = (req, res) => {
 
-    bcrypt.hash(req.body.password, 10, function (err, hash){
+  var register = new User({
+    'name' : req.body.name,
+    'registration' : req.body.registration,
+    'sector' : req.body.sector,
+    'hospital' : req.body.hospital,
+    'password' : req.body.password,
+    'manager' : req.body.manager
+  });
 
-      if (err) {
-        return res.json({
-          message: 'password not found.'
-        });
-      }
-      if (req.body.name, req.body.registration, req.body.sector, req.body.hospital, req.body.password, req.body.manager) {
+  register.save(function (err) {
 
-  		var register = new User({
-  			'name' : req.body.name,
-  			'registration' : req.body.registration,
-        'sector' : req.body.sector,
-        'hospital' : req.body.hospital,
-  			'password' : hash,
-  			'manager' : req.body.manager
-        });
+    if (err) {
+      console.log(err);
+      res.end();
+    }
+  });
+          
+  res.send(register);
+  res.end();
 
-  		register.save(function (err) {
-
-  			if (err) {
-  				console.log(err);
-  				res.end();
-  			}
-  		});
-  	res.send(register);
-  	res.end();
-
-   } else{
-    res.json({
-      success: false,
-      message: 'something is missing or wrong.'
-    });
-   }
-
-    });
-  }
+}
