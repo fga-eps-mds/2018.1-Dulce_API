a4493d9c016215565bb8090a1098b153f67387ff
Guilherme Deusdará <guibanci@gmail.com>
2018-04-11 23:00:03 -0300

    HOTFIX #17 arrumando rotas que requerem autentificação
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Signed-off-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>
    
    Co-authored-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Ezequiel de Oliveira <ezequiel1de1oliveira@gmail.com>

--- User/app/app.js
+++ User/app/app.js
@@ -26,29 +26,27 @@ console.log(mongoaddr);
 mongo.connect(mongoaddr);
 
 
+
 app.post('/user/add', newUser);
-app.get('/user/all', allUsers);
 app.post('/user/login', login);
-app.get('/user/view/:id', viewUser);
-app.put('/user/edit/:id', editUser);
 
 appRoutes = express.Router();
 
 appRoutes.use((req, res, next) => {
 
     // check header or post parameters for token
-    var token = req.body.token || req.headers['Bearer'];
+    var token = req.headers['x-access-token'];
   
     // decode token
     if (token) {
   
       // verifies secret and checks exp
-      jwt.verify(token, app.get(SECRET_KEY), function(err, decoded) {      
+      jwt.verify(token, SECRET_KEY, function(err, decoded) {      
         if (err) {
           return res.json({ success: false, message: 'Failed to authenticate token.' });    
         } else {
           // if everything is good, save to request for use in other routes
-          req.decoded = decoded;    
+              
           next();
         }
       });
@@ -64,9 +62,17 @@ appRoutes.use((req, res, next) => {
     
     }
 });
-    
-app.use('/main', appRoutes);
+   
+app.use('/', appRoutes);
   
+
+app.get('/user/view/:id', viewUser);
+app.put('/user/edit/:id', editUser);
+app.get('/user/all', allUsers);
+
+
+ 
+
 
 app.listen(8080, function() {
 	console.log('Funcionando');
