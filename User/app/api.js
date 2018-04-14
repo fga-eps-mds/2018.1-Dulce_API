34a39a55ecdf2d3254497ac5429dd2e774ab3c45
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2018-04-14 18:42:01 -0300

    Incluindo mais ações no serviço de manter usuário
    Signed-off-by: Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>

--- User/app/api.js
+++ User/app/api.js
@@ -1,36 +1,43 @@
 module.exports = function api(options) {
 
-  var valid_ops = { create:'create' }
 
-  this.add('role:api,path:userManager', function (msg, respond) {
+  this.add('role:api,path:create', function (msg, respond) {
 
-    console.log(msg)
-    var operation = msg.args.params.operation
     var name = msg.args.body.name
     var registration = msg.args.body.registration
     var sector = msg.args.body.sector
     var hospital = msg.args.body.hospital
     var password = msg.args.body.password
     var manager = msg.args.body.manager
+    var id = msg.args.query.id
 
-    this.act('role:user', {
-      cmd:   valid_ops[operation],
-      operation: operation,
+    this.act('role:user,cmd:create', {
       name: name,
       registration: registration,
       sector: sector,
       hospital: hospital,
       password: password,
       manager: manager,
+      id: id
     }, respond)
   })
 
+  this.add('role:api,path:listById',function(msg, respond){
+
+    var id = msg.args.query.id
+
+    this.act('role:user, cmd:listById', {
+      id: id
+    }, respond)
+  });
+
   this.add('init:api', function (msg, respond) {
-    this.act('role:web',{routes:{
-      prefix: '/api',
+    this.act('role:web',{ routes: {
+      prefix: '/api/userManager',
       pin:    'role:api,path:*',
       map: {
-        userManager: { POST:true, suffix:'/:operation' }
+        create: { POST:true },
+        listById: { GET:true }
       }
     }}, respond)
   })
---
+++ User/app/user.js
@@ -0,0 +1,29 @@
+module.exports = function User(options) {
+
+  this.add('role:user, cmd:create', function create( msg, respond ) {
+
+      var user = this.make('users')
+
+      user.name = msg.name
+      user.registration = msg.registration
+      user.sector = msg.sector
+      user.hospital = msg.hospital
+      user.password = msg.password
+      user.manager = msg.manager
+
+      user.save$(function(err,user){
+        console.log(user)
+        respond( null, user)
+    })
+});
+
+this.add('role:user, cmd:listById', function listById (msg, respond){
+
+      var userId = msg.id;
+      var user = this.make('users')
+	     user.load$(userId, function(error, user) {
+		       respond(null, user);
+	});
+});
+
+}
