2e430393f4cc3ba333a9e0f4d60a19d8de8d5d4e
Vitor Leal <vitinho2201@gmail.com>
2018-04-27 21:50:10 -0300

    FIX #29 Retirando testes utilizados como base
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>

--- Sector/Client/test/sector.test.js
+++ Sector/Client/test/sector.test.js
@@ -5,27 +5,6 @@ var should = require('should');
 
 describe('Routing', function() {
   const url = 'http://localhost:8084';
-  /*
-
-  describe('should test new sector creation', () => {
-    it('should create a new sector',(done) => {
-      let sector = {
-        name: 'New Sector'
-      };
-	request(url)
-		.post('/api/sector/create')
-		.send(sector)
-		.end((err,res) => {
-			if (err) {
-				throw err;
-			}
-	    res.args.body.name.should.equal('New Sector');
-      //res.status.should.equal(200);
-			done();
-		});
-	});
-});
-*/
 
 describe('should test the token validation', () => {
   it('should return an error message: access denied', () => {
@@ -42,61 +21,6 @@ describe('should test the token validation', () => {
     res.status.should.equal(403);
      });
    });
-
 });
-/*
 
-
-describe('/POST Register', () => {
-  it('it should Register, Login, and check token', () => {
-    let login_details = {
-      'registration': 'bosta',
-      'password': '1234'
-    }
-
-    let register_details = {
-      'name': 'John',
-      'registration': '54321',
-      'hospital': 'Gama',
-      'sector' : '1234',
-      'password': '1234',
-      'manager': true
-    };
-    request(url)
-      .post('/user/add')
-      .send(register_details) // this is like sending a post with a new User
-      .end((err, res) => { // when we get a response from the endpoint
-        // in other words,
-        // the res object should have a status of 201
-        res.should.have.status(201);
-        // the property, res.body.state, we expect it to be true.
-        expect(res.body.state).to.be.true;
-
-        // follow up with login
-        request(url)
-          .post('/user/login')
-          .send(login_details)
-          .end((err, res) => {
-            console.log('this was run the login part');
-            res.should.have.status(200);
-            expect(res.body.state).to.be.true;
-            res.body.should.have.property('token');
-
-            let token = res.body.token;
-            // follow up with requesting user protected page
-              request(url)
-              .get('/user/all')
-              // we set the auth header with our token
-              .set('x-access-token', token)
-              .end((err, res) => {
-                res.status.should.equal(200);
-                expect(res.body.state).to.be.true;
-                res.body.data.should.be.an('array');
-              })
-          })
-
-      })
-    })
-  })
-*/
 });
